1df6b5b7b8769ae591e7193e98865a8a8e4a25e2
maki-engineer <ryohei-81@outlook.jp>
2022-12-13 11:47:26 +0900

    update: 問題不正解

--- practice.py
+++ practice.py
@@ -1,13 +1,59 @@
-A = int(input())  # 500円玉の枚数
-B = int(input())  # 100円玉の枚数
-C = int(input())  # 50円玉の枚数
-X = int(input())  # 合計金額
+H, W = map(int, input().split())
+S    = []
 
-count = 0
+for i in range(H):
+  S.append(list(input()))
 
-for a in range(A + 1):
-  for b in range(B + 1):
-    for c in range(C + 1):
-      if (500 * a) + (100 * b) + (50 * c) == X: count += 1
+for h in range(H):
+  for w in range(W):
+    if h == 0:
+      if w == 0:
+        if S[h][w] != '#':
+          string = S[h][w + 1] + S[h + 1][w] + S[h + 1][w + 1]
 
-print(count)
+          S[h][w] = str(string.count('#'))
+      elif w == W - 1:
+        if S[h][w] != '#':
+          string = S[h][w - 1] + S[h + 1][w - 1] + S[h + 1][w]
+
+          S[h][w] = str(string.count('#'))
+      else:
+        if S[h][w] != '#':
+          string = S[h][w - 1] + S[h][w + 1] + S[h + 1][w - 1] + S[h + 1][w] + S[h + 1][w + 1]
+
+          S[h][w] = str(string.count('#'))
+    elif h == H - 1:
+      if w == 0:
+        if S[h][w] != '#':
+          string = S[h - 1][w] + S[h - 1][w + 1] + S[h][w + 1]
+
+          S[h][w] = str(string.count('#'))
+      elif w == W - 1:
+        if S[h][w] != '#':
+          string = S[h - 1][w - 1] + S[h - 1][w] + S[h][w - 1]
+
+          S[h][w] = str(string.count('#'))
+      else:
+        if S[h][w] != '#':
+          string = S[h - 1][w - 1] + S[h - 1][w] + S[h - 1][w + 1] + S[h][w - 1] + S[h][w + 1]
+
+          S[h][w] = str(string.count('#'))
+    else:
+      if w == 0:
+        if S[h][w] != '#':
+          string = S[h - 1][w] + S[h - 1][w + 1] + S[h][w + 1] + S[h + 1][w] + S[h + 1][w + 1]
+
+          S[h][w] = str(string.count('#'))
+      elif w == W - 1:
+        if S[h][w] != '#':
+          string = S[h - 1][w - 1] + S[h - 1][w] + S[h][w - 1] + S[h + 1][w - 1] + S[h + 1][w]
+
+          S[h][w] = str(string.count('#'))
+      else:
+        if S[h][w] != '#':
+          string = S[h - 1][w - 1] + S[h - 1][w] + S[h - 1][w + 1] + S[h][w - 1] + S[h][w + 1] + S[h + 1][w - 1] + S[h + 1][w] + S[h + 1][w + 1]
+
+          S[h][w] = str(string.count('#'))
+
+for i in range(H):
+  print("".join(S[i]))
