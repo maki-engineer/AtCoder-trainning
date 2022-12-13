327ada35bcccf50019311e441f0502925161159a
maki-engineer <ryohei-81@outlook.jp>
2022-12-13 10:21:52 +0900

    update: 問題正解

--- practice.py
+++ practice.py
@@ -4,10 +4,10 @@ check = False
 
 if s[0] != 'A': check = True
 
-if 'C' not in s[2:-2]:
+if s[2:-1].count('C') != 1:
   check = True
 else:
-  ci    = s[2:-2].find('C')
+  ci    = s[2:-1].find('C')
   s     = s[1:ci + 2] + s[ci + 3:]
 
 if not s.islower(): check = True
