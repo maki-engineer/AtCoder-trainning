4d357a6c74b9391626fec35712026ada37814a49
maki-engineer <ryohei-81@outlook.jp>
2022-12-14 20:38:29 +0900

    update: 問題解決

--- ABC085C.py
+++ ABC085C.py
@@ -1,2 +1,10 @@
 N, Y = map(int, input().split())
 
+a, b, c = -1, -1, -1
+
+for i in range(N + 1):
+  for j in range(N + 1):
+    if (i) + (j) + (abs(N - i - j)) == N and (10000 * i) + (5000 * j) + (1000 * (N - i - j)) == Y:
+      a, b, c = i, j, N - i - j
+
+print(a, b, c)
