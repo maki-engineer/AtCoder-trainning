5501f7991f58b59128747c58d2539ac2cd03b232
maki-engineer <ryohei-81@outlook.jp>
2022-12-18 14:47:38 +0900

    update: 問題不正解

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,10 +1,13 @@
-N, X, T = map(int, input().split())
+H, W = map(int, input().split())
+
+S = [['.'] * W for i in range(H)]
 
 result = 0
-count  = 0
 
-while count < N:
-  count  += X
-  result += T
+for h in range(H):
+  for w in range(W):
+    if h % 2 == 0:
+      if w % 2 == 0:
+        result += 1
 
 print(result)
