0e16d51a07926511610cae09841b2be0137812b1
maki-engineer <ryohei-81@outlook.jp>
2022-12-19 19:28:35 +0900

    update: 問題不正解

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,6 +1,17 @@
-K    = int(input())
-A, B = map(str, input().split())
+def base10int(value, base):
+  if (int(value / base)):
+    return base10int(int(value / base), base) + str(value % base)
+  return str(value % base)
 
-a, b = int(A, K), int(B, K)
+N, K = map(str, input().split())
+N    = int(N, 8)
+K    = int(K)
 
-print(a * b)
+for i in range(K):
+  N = base10int(N, 9)
+  N = N.replace('8', '5')
+  N = int(N, 8)
+
+N = base10int(N, 8)
+
+print(N)
