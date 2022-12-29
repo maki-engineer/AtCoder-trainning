4fd4d0c1cb03e744ca538d57d9e77c126a87edff
maki-engineer <ryohei-81@outlook.jp>
2022-12-29 21:04:08 +0900

    update: 問題不正解

--- Practice/ABC049C.py
+++ Practice/ABC049C.py
@@ -1,1 +1,37 @@
-S = input()
+S, str_len, check = input(), 5, False
+
+while S:
+  check_str = S[:str_len]
+
+  if str_len == 5:
+    if (check_str == 'dream') or (check_str == 'erase'):
+      check = True
+
+    str_len += 1
+
+  elif str_len == 6:
+    if check_str == 'eraser':
+      S       = S[str_len:]
+      str_len = 5
+    else:
+      if check:
+        str_len = 5
+        S       = S[str_len:]
+        check   = False
+      else:
+        str_len += 1
+
+  else:
+    if check_str != 'dreamer':
+      if check:
+        str_len = 5
+        S       = S[str_len:]
+        check   = False
+      else:
+        print('NO')
+        exit()
+    else:
+      S       = S[str_len:]
+      str_len = 5
+
+print('YES')
